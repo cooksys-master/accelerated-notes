ca1d172cd786376f9b3270d7492019f270422ec9
Will Marttala <wmarttala123@gmail.com>
2021-01-11 16:54:01 -0600

    Completed the collections assignment

--- collections/src/main/java/com/cooksys/ftd/assignments/collections/model/Worker.java
+++ collections/src/main/java/com/cooksys/ftd/assignments/collections/model/Worker.java
@@ -1,88 +1,122 @@
 package com.cooksys.ftd.assignments.collections.model;
 
-import com.cooksys.ftd.assignments.collections.util.MissingImplementationException;
-
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * TODO: Implement this class
- *  <br><br>
- *  A worker is a type of employee that cannot be a superior to another employee.
- *  <br>
- *  A worker should have a name, and, optionally, a manager superior to them.
+ * TODO: Implement this class <br>
+ * <br>
+ * A worker is a type of employee that cannot be a superior to another employee.
+ * <br>
+ * A worker should have a name, and, optionally, a manager superior to them.
  */
 public class Worker implements Employee {
 
-    // TODO: Does this class need private fields? If so, add them here
+	private String name;
+	private Manager manager;
 
-    /**
-     * TODO: Implement this constructor.
-     *
-     * @param name the name of the worker to be created
-     */
-    public Worker(String name) {
-        throw new MissingImplementationException();
-    }
+	/**
+	 *
+	 * @param name the name of the worker to be created
+	 */
+	public Worker(String name) {
+		this.name = name;
+	}
 
-    /**
-     *  TODO: Implement this constructor.
-     *
-     * @param name the name of the worker to be created
-     * @param manager the direct manager of the worker to be created
-     */
-    public Worker(String name, Manager manager) {
-        throw new MissingImplementationException();
-    }
+	/**
+	 *
+	 * @param name    the name of the worker to be created
+	 * @param manager the direct manager of the worker to be created
+	 */
+	public Worker(String name, Manager manager) {
+		this.name = name;
+		this.manager = manager;
+	}
 
-    /**
-     * TODO: Implement this getter.
-     *
-     * @return the name of the worker
-     */
-    @Override
-    public String getName() {
-        throw new MissingImplementationException();
-    }
+	/**
+	 *
+	 * @return the name of the worker
+	 */
+	@Override
+	public String getName() {
+		return name;
+	}
 
-    /**
-     * TODO: Implement this getter.
-     *
-     * @return {@code true} if this worker has a manager, or {@code false} otherwise
-     */
-    @Override
-    public boolean hasManager() {
-        throw new MissingImplementationException();
-    }
+	/**
+	 *
+	 * @return {@code true} if this worker has a manager, or {@code false} otherwise
+	 */
+	@Override
+	public boolean hasManager() {
+		return manager != null;
+	}
 
-    /**
-     * TODO: Implement this getter.
-     *
-     * @return the manager of this worker, or null if it has none
-     */
-    @Override
-    public Manager getManager() {
-        throw new MissingImplementationException();
-    }
+	/**
+	 *
+	 * @return the manager of this worker, or null if it has none
+	 */
+	@Override
+	public Manager getManager() {
+		return manager;
+	}
 
-    /**
-     * TODO: Implement this method.
-     *  <br><br>
-     *  Retrieves the worker's chain of command as a {@code List<Manager>}, starting with their direct {@code Manager},
-     *  followed by that {@code Manager}'s {@code Manager}, and so on, until the top of the hierarchy is reached.
-     *  <br><br>
-     *  The returned list should never be or contain {@code null}.
-     *  <br><br>
-     *  If the worker does not have a {@code Manager}, an empty
-     *  {@code List<Manager>} should be returned.
-     *
-     * @return a {@code List<Manager>} that represents the worker's chain of command,
-     */
-    @Override
-    public List<Manager> getChainOfCommand() {
-        throw new MissingImplementationException();
-    }
+	/**
+	 * <br>
+	 * Retrieves the worker's chain of command as a {@code List<Manager>}, starting
+	 * with their direct {@code Manager}, followed by that {@code Manager}'s
+	 * {@code Manager}, and so on, until the top of the hierarchy is reached. <br>
+	 * <br>
+	 * The returned list should never be or contain {@code null}. <br>
+	 * <br>
+	 * If the worker does not have a {@code Manager}, an empty {@code List<Manager>}
+	 * should be returned.
+	 *
+	 * @return a {@code List<Manager>} that represents the worker's chain of
+	 *         command,
+	 */
+	@Override
+	public List<Manager> getChainOfCommand() {
+		List<Manager> result = new ArrayList<>();
+		for (Manager manager = getManager(); manager != null; manager = manager.getManager()) {
+			result.add(manager);
+		}
+		return result;
+	}
 
-    // TODO: Does this class need custom .equals() and .hashcode() methods? If so, implement them here.
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((manager == null) ? 0 : manager.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
 
-    // TODO [OPTIONAL]: Consider adding a custom .toString() method here if you want to debug your code with System.out.println() statements
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Worker other = (Worker) obj;
+		if (manager == null) {
+			if (other.manager != null)
+				return false;
+		} else if (!manager.equals(other.manager))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Worker {name=" + name + ", manager=" + manager + "}";
+	}
+
 }
